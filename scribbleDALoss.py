c1e396322064d1bcbd1e782e745bd26bded5bcb7
ReubenDo <reuben.dorent@kcl.ac.uk>
2021-06-14 18:11:36 +0100

    Adding the option without normalization

--- scribbleDALoss.py
+++ scribbleDALoss.py
@@ -12,7 +12,8 @@ class CRFLoss(nn.Module):
                  beta=5.,
                  gamma=5.,
                  w=[1.0,0.0],
-                 is_da=False):
+                 is_da=False,
+                 use_norm=True):
         super(CRFLoss, self).__init__()
         self._alpha = alpha
         self._beta = beta
@@ -20,19 +21,17 @@ class CRFLoss(nn.Module):
         self._w = w
         self.pl = pl.apply
         self.is_da = is_da
+        self.use_norm = use_norm
 
     def forward(self, I, U):
         """
-        Compute `T` iterations of mean field update given a dense CRF.
-        This layer maintains has NO trainable parameters
-        (neither a compatibility function nor `m` kernel weights).
         :param I: feature maps used in the dense pairwise term of CRF
         :param U: activation maps used in the unary term of CRF (before Softmax)
-        :return: Maximum a posteriori labeling (before Softmax)
+        :return: CRF loss
         """
         batch_size, n_feat, x_shape, y_shape, z_shape = I.size()
         nb_voxels = x_shape*y_shape*z_shape 
-        spatial_rank = 3
+        spatial_rank = 3 #3D loss
         n_ch = U.size(1)
         
         spatial_x, spatial_y, spatial_z = torch.meshgrid(torch.arange(x_shape).cuda(), 
@@ -45,33 +44,25 @@ class CRFLoss(nn.Module):
             spatial_coords = torch.stack([spatial_x, spatial_y, spatial_z], 0)
             spatial_coords = spatial_coords.unsqueeze(0).repeat(batch_size, 1, 1, 1, 1)
             spatial_coords = spatial_coords.type(torch.cuda.FloatTensor).detach()
-            bilateral_coords = torch.cat([spatial_coords / self._alpha, I / self._beta], 1)
+            features = torch.cat([spatial_coords / self._alpha, I / self._beta], 1)
 
         else:
-            bilateral_coords = I / self._beta
+            features = I / self._beta
             
         if not self.is_da:
             if self._alpha>0:
-                bilateral_coords = torch.reshape(bilateral_coords, (batch_size, n_feat + spatial_rank, -1))
+                features = torch.reshape(features, (batch_size, n_feat + spatial_rank, -1))
             else:
-                bilateral_coords = torch.reshape(bilateral_coords, (batch_size, n_feat, -1))
+                features = torch.reshape(features, (batch_size, n_feat, -1))
             ones = torch.ones(batch_size, 1, nb_voxels, device='cuda') 
             
         else:
-            bilateral_coords = torch.reshape(bilateral_coords, (1, n_feat, -1))
+            features = torch.reshape(features, (1, n_feat, -1))
             ones = torch.ones(1, 1, batch_size*nb_voxels, device='cuda')
 
-
-
-        features = [bilateral_coords]
-
-
-        norms = []
-        
-        for idx, feat in enumerate(features):
-            spatial_norm = self.pl(feat, ones)
+        if self.use_norm:
+            spatial_norm = self.pl(bilateral_coords, ones)
             spatial_norm = 1.0 / torch.sqrt(spatial_norm + 1e-20)
-            norms.append(spatial_norm)
         
         if not self.is_da:
             U = torch.reshape(U, [batch_size, n_ch, -1])
@@ -82,10 +73,12 @@ class CRFLoss(nn.Module):
             H1 = torch.nn.Softmax(1)(U)
         else:
             H1 = U
-        Q1 = 0
-        for idx, feat in enumerate(features):
-            Q = self.pl(feat, H1 * norms[idx])
-            Q1 += Q * norms[idx]
+        
+        if self.use_norm:
+            Q1 = self.pl(features, H1 * spatial_norm)
+            Q1 = Q1 * spatial_norm
+        else:
+            Q1 = self.pl(features, H1)
 
         Q1 = torch.reshape(Q1, [-1, n_ch])
         H1 = torch.reshape(torch.nn.Softmax(1)(U), [-1, n_ch])
